a51c2369a21798c6e03139c13904535b0a1b4bd4
arthurkou <arthur_kou@hotmail.com>
2023-05-06 07:59:16 -0300

    Migration for Person Entity

---
+++ rest-api-springboot-cloud-artkou/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` VALUES
+    (1,'Carlos','Silva','Brasil','Male'),
+    (2,'André','Andrade','Portugal','Male'),
+    (4,'Robson','Abreu','França','Male'),
+    (5,'José','Jr','Bélgica','Male'),
+    (6,'João','Carlos','Suiça','Male'),
+    (7,'Marina','Silva','Alemanha','Female'),
+    (8,'Cláudia','Gamma','Espanha','Female'),
+    (9,'Ana','Maria','Holanda','Female'),
+    (11,'Fábio','Júnior','Brasil','Male')
